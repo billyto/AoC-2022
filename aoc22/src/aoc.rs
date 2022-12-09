15c6b11985c6df5d9f66304c34ca3e4dc27927b5
Billy Tobon <billy.tobon@gmail.com>
2022-12-08 23:01:47 -0500

    day 8 completed

--- aoc22/src/aoc.rs
+++ aoc22/src/aoc.rs
@@ -2,6 +2,8 @@ use std::fs;
 use regex::Regex;
 use std::str::FromStr;
 use itertools::{Itertools, Unique};
+use indextree::{Arena, NodeEdge, NodeId};
+use std::fmt;
 
 extern crate array_tool;
 
@@ -359,5 +361,103 @@ pub fn day6(){
             break;
         } 
     }
+
+}
+
+
+#[derive(Copy, Clone)]
+struct NodeInfo<'a> {
+    name: &'a str, 
+    size: usize}
+
+impl fmt::Display for NodeInfo<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "node: {}, size: {}", self.name, self.size)
+    }
+}
+impl PartialEq for NodeInfo<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.name.eq(other.name) && self.size == other.size
+    }
+}
+
+pub fn day7(){
+
+    let contents = fs::read_to_string("input/07-input").expect("problem with the file");
+    let instructions: Vec<&str> = contents.split("\n").collect();
+    let arena = &mut Arena::new();    
+    let mut pointer = arena.new_node(NodeInfo{name:"/",size:0}); // root
+    let root = pointer.clone();
+
+    for inst in instructions {
+
+        if inst.starts_with("$ cd"){
+                // got to dir
+            let dir_dest = inst.strip_prefix("$ cd ").unwrap().trim();
+
+            match dir_dest {
+                ".." =>  { 
+                        pointer = arena.get(pointer).unwrap().parent().unwrap();
+                }, //move one up
+                "/" => {}, // go to root
+                _ => { // got to dir_dest
+                    pointer = pointer.children(&arena).filter(|child| 
+                        arena.get(*child).unwrap().get().name.eq(dir_dest)).next().unwrap();
+                }, 
+            }
+
+        } else if inst.starts_with("dir") {
+            let dir_name = inst.strip_prefix("dir ").unwrap().trim();
+                let dir = arena.new_node(NodeInfo{name: dir_name, size:0});
+                pointer.append(dir, arena)//create a dir
+        } else if inst.starts_with("$ ls") {
+                // following lines are in current node
+        } else {
+            // this is a file ie. 324234 b.txt
+            let temp_vec = inst.split(" ").collect::<Vec<&str>>();
+            let file_size:usize =  FromStr::from_str(temp_vec.first().unwrap()).unwrap();
+            let file_name = temp_vec.last().unwrap();
+            let file = arena.new_node(NodeInfo{name: file_name, size: file_size});
+            pointer.append(file, arena);
+
+            let oldies = pointer.ancestors(&arena).collect::<Vec<NodeId>>();
+            for ancestor in oldies {
+                let node_info = arena.get_mut(ancestor).unwrap().get_mut();
+                node_info.size = node_info.size + file_size;
+            }
+        }
+    }
+    println!("dir tree is\n {}", root.debug_pretty_print(&arena));
+    let traverser = root.traverse(arena);
+    
+    let mut dirs: Vec<NodeInfo> = Vec::new();
+    for node_e in traverser {
+        let n =  match node_e {
+            NodeEdge::Start(node_info) => node_info,
+            NodeEdge::End(node_info) => node_info,
+        };
+        let node_info = arena.get(n).unwrap().get();
+        let node_name = node_info.name;
+        let node_size = node_info.size;
+        
+        if !dirs.contains(node_info) && n.children(arena).count() > 0 { // &&  node_size <=100000{  // enable for part 1
+            println!("adding {}", node_info);
+            dirs.push(*node_info);
+        }
+    }
+
+    let all_smalls = dirs.iter()
+                .fold(0,|accum, node| accum + node.size);
+
+    println!("Big number is {}", all_smalls);
+
+    let space_taken = arena.get(root).unwrap().get().size;
+    let free_space: usize = 70000000 - space_taken;
+    let space_needed: usize = 30000000 - free_space;
+    println!("Free space {}, space needed {}", free_space, space_needed); 
+
+    let candidates = dirs.iter().filter(|node| node.size > space_needed);
+    let min = candidates.min_by(|x,t|  x.size.cmp(&t.size)).unwrap(); 
+    println!("best option is {}", min);
 
 }
--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,100 +1,192 @@
-use std::fs;
-use indextree::{Arena, NodeEdge, NodeId};
-use std::str::FromStr;
-use std::fmt;
-
-#[derive(Copy, Clone)]
-struct NodeInfo<'a> {
-    name: &'a str, 
-    size: usize}
-
-impl fmt::Display for NodeInfo<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "node: {}, size: {}", self.name, self.size)
-    }
-}
-impl PartialEq for NodeInfo<'_> {
-    fn eq(&self, other: &Self) -> bool {
-        self.name.eq(other.name) && self.size == other.size
-    }
-}
+use std::{fs, ops::Index};
 
 fn main() {
 
-    let contents = fs::read_to_string("input/07-input").expect("problem with the file");
-    let instructions: Vec<&str> = contents.split("\n").collect();
-    let arena = &mut Arena::new();    
-    let mut pointer = arena.new_node(NodeInfo{name:"/",size:0}); // root
-    let root = pointer.clone();
+    let contents = fs::read_to_string("input/08-input").expect("problem with the file");    
 
-    for inst in instructions {
+    let forest = contents.lines()
+                .map(|line| line.chars()
+                        .map(|c|  c.to_digit(10).unwrap()).collect::<Vec<u32>>()).collect::<Vec<Vec<u32>>>();
 
-        if inst.starts_with("$ cd"){
-                // got to dir
-            let dir_dest = inst.strip_prefix("$ cd ").unwrap().trim();
 
-            match dir_dest {
-                ".." =>  { 
-                        pointer = arena.get(pointer).unwrap().parent().unwrap();
-                }, //move one up
-                "/" => {}, // go to root
-                _ => { // got to dir_dest
-                    pointer = pointer.children(&arena).filter(|child| 
-                        arena.get(*child).unwrap().get().name.eq(dir_dest)).next().unwrap();
-                }, 
+
+    //Part 2
+    let mut scores :Vec<u32> = Vec::new();
+    for (i, tree_row) in forest.iter().enumerate(){
+       
+        let mut tree_score: u32 = 0;
+        for (j,tree) in tree_row.iter().enumerate() {
+
+            println!("indexes i:{i} j:{j}");
+
+            //check the north
+            let mut north_score = 0;
+            let north_score_view = forest[..i].iter()
+                .map(|v_row| v_row.get(j).unwrap());
+
+            if i == 1 || i == 0{
+                north_score = 1; //  only have one tree to see no matter the heigh 
+            } else{
+                for (index, x) in north_score_view.enumerate().rev() {
+                    println!("{}:vec north x is {}",index, x);    
+                    north_score = north_score +1;
+                    if x>= tree {
+                        break;
+                    }
+                }
             }
 
-        } else if inst.starts_with("dir") {
-            let dir_name = inst.strip_prefix("dir ").unwrap().trim();
-                let dir = arena.new_node(NodeInfo{name: dir_name, size:0});
-                pointer.append(dir, arena)//create a dir
-        } else if inst.starts_with("$ ls") {
-                // following lines are in current node
-        } else {
-            // this is a file ie. 324234 b.txt
-            let temp_vec = inst.split(" ").collect::<Vec<&str>>();
-            let file_size:usize =  FromStr::from_str(temp_vec.first().unwrap()).unwrap();
-            let file_name = temp_vec.last().unwrap();
-            let file = arena.new_node(NodeInfo{name: file_name, size: file_size});
-            pointer.append(file, arena);
+            //check the south
+            let mut south_score: u32 = 0;
+            let south_score_view = forest[i+1..].iter()
+            .map(|v_row| v_row.get(j).unwrap());
+            
+            if i == forest.len()-1 || i == forest.len() -2 {
+                south_score = 1;
+            } else{
+                for (index, x) in south_score_view.enumerate() {
+                    println!("{}:vec south x is {}",index, x);    
+                    south_score = south_score +1;
+                    if x>= tree {
+                        break;
+                    }
+                }
+            }
 
-            let oldies = pointer.ancestors(&arena).collect::<Vec<NodeId>>();
-            for ancestor in oldies {
-                let node_info = arena.get_mut(ancestor).unwrap().get_mut();
-                node_info.size = node_info.size + file_size;
+            //check the west
+            let west_score_view = tree_row[..j].iter();
+            let mut west_score: u32 = 0;
+            if j == 0 {
+                west_score = 1;
+            }else {
+                for (index, x) in west_score_view.rev().enumerate(){
+                    println!("{}:vec west x is {}",index, x);    
+                    west_score = west_score +1;
+                    if x>= tree {
+                        break;
+                    }  
+                }
+            }
+
+            //check the east
+            let east_score_view = tree_row[j+1..].iter();
+            let mut east_score: u32 = 0;
+            if j == tree_row.len()-1 {
+                east_score = 1;
+            } else {
+                for (index, x) in east_score_view.enumerate(){
+                    println!("{}:vec east x is {}",index, x);    
+                    east_score = east_score +1;
+                    if x>= tree {
+                        break;
+                    } 
+                }
+            }
+
+            tree_score = north_score * south_score * west_score * east_score;
+            scores.push(tree_score);
+ 
+        }
+    }
+    println!("Max scores is {}", scores.iter().max().unwrap());
+
+}
+
+fn day8_p1(){
+    let contents = fs::read_to_string("input/08-sample").expect("problem with the file");    
+
+    let forest = contents.lines()
+                .map(|line| line.chars()
+                        .map(|c|  c.to_digit(10).unwrap()).collect::<Vec<u32>>()).collect::<Vec<Vec<u32>>>();
+
+    // println!("forest is {}", forest.len());
+
+    let mut visible_trees: usize = 0; //forest.len() + forest[0].len(); // start with the perimeter
+    println!("Perimeter trees (visible) {visible_trees}");
+
+    //Part 1 
+    let mut scores :Vec<u32> = Vec::new();
+    for (i, tree_row) in forest.iter().enumerate(){
+
+        if i ==0 || i == forest.len()-1 {
+       //     println!("Skipping checking treeline {}, all trees are visible", i);
+            visible_trees = visible_trees + tree_row.len();
+            continue;
+        }
+
+       
+        for (j,tree) in tree_row.iter().enumerate() {
+
+            let mut north_score = 0;
+            if j == 0 || j == tree_row.len()-1 {
+       //         println!("Skipping check for tree {i},{j} w height {tree}, always visible");
+                visible_trees = visible_trees + 1;
+            } else{
+                println!("Checking tree {i},{j} w height {tree}");
+
+                //check north view
+                let north_view  = forest[..i].iter()
+                       .map(|v_row| v_row.get(j).unwrap())
+                               .filter(|v_tree| *v_tree  >= tree ).count() == 0;
+
+                println!("indexes i:{i} j:{j}");
+                let north_score_view = forest[..i].iter()
+
+                .map(|v_row| v_row.get(j).unwrap());
+
+
+                if i == 1 {
+                    north_score = 1; //  only have one tree to see no matter the heigh 
+                }
+                else{
+                   for (index, x) in north_score_view.enumerate().rev() {
+                        println!("{}:vec x is {}",index, x);
+                        
+                        north_score = north_score +1;
+                        if x>= tree {
+                            break;
+                        }
+                        
+                    }
+                }
+                scores.push(north_score);
+
+                if north_view { 
+          //          println!(" -> visible from north");
+                    visible_trees = visible_trees + 1;
+                    continue;
+                }
+
+                let south_view = forest[(
+                    i+1)..].iter()
+                        .map(|v_row| v_row.get(j).unwrap())
+                            .filter(|v_tree| *v_tree  >= tree ).count() == 0 ;
+
+                if south_view {
+        //            println!(" -> visible from south"); 
+                    visible_trees = visible_trees + 1;
+                    continue;
+                }
+
+                let _temp_west = tree_row[..j].to_vec();
+                let west_view = tree_row[..j].iter().filter(|t| *t >= tree).count() == 0;
+
+                if west_view {
+       //             println!(" -> visible from west");
+                    visible_trees = visible_trees + 1;
+                    continue;
+                }
+
+                let _temp = tree_row[(j+1)..].to_vec();
+                let east_view = tree_row[(j+1)..].iter().filter(|t| *t >= tree).count() == 0 ;
+
+                if east_view {
+        //            println!(" -> visible from east");
+                    visible_trees = visible_trees + 1;
+                    continue;
+                }
             }
         }
     }
-    println!("dir tree is\n {}", root.debug_pretty_print(&arena));
-    let traverser = root.traverse(arena);
-    
-    let mut dirs: Vec<NodeInfo> = Vec::new();
-    for node_e in traverser {
-        let n =  match node_e {
-            NodeEdge::Start(node_info) => node_info,
-            NodeEdge::End(node_info) => node_info,
-        };
-        let node_info = arena.get(n).unwrap().get();
-        let node_name = node_info.name;
-        let node_size = node_info.size;
-        
-        if !dirs.contains(node_info) && n.children(arena).count() > 0 { // &&  node_size <=100000{  // enable for part 1
-            println!("adding {}", node_info);
-            dirs.push(*node_info);
-        }
-    }
-
-    let all_smalls = dirs.iter()
-                .fold(0,|accum, node| accum + node.size);
-
-    println!("Big number is {}", all_smalls);
-
-    let space_taken = arena.get(root).unwrap().get().size;
-    let free_space: usize = 70000000 - space_taken;
-    let space_needed: usize = 30000000 - free_space;
-    println!("Free space {}, space needed {}", free_space, space_needed); 
-
-    let candidates = dirs.iter().filter(|node| node.size > space_needed);
-    let min = candidates.min_by(|x,t|  x.size.cmp(&t.size)).unwrap(); 
-    println!("best option is {}", min);
+    println!("All Visible trees {visible_trees}");
 }
